316caa32bb5e982b1a10c0c3a9e82faa59dc1508
RLSMname <115074855+RLSMname@users.noreply.github.com>
2024-03-28 15:09:24 +0200

    Added sort module

---
+++ board_games/board_games/src/Sort/Sorter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace board_games.src.Sort
+{
+    public class Sorter<T> where T : IComparable<T>
+    {
+        private SortStrategy<T>? strategy = null;
+        public Sorter() { }
+        public void SetStrategy(SortStrategy<T> strategy)
+        {
+            this.strategy = strategy;
+        }
+        public void Sort(List<T> data) {
+            if(strategy != null)
+            {
+                strategy.Sort(data);
+            }
+            else
+            {
+                throw new Exception("Strategy not selected");
+            }
+        }
+    }
+}
